f16f7bfef0130579a907dc395c79ff7caf9d1347
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-09-30 19:20:16 +0200

    Add a test on nullable column

--- column_type/data/data.sql
+++ column_type/data/data.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS boats;
 DROP TABLE IF EXISTS cars;
+drop function IF EXISTS boats_migr01_trg ;
+drop function IF EXISTS cars_migr01_trg ;
+
 
 CREATE TABLE boats (id serial primary key, price numeric(7,2) default 3.14,
-discount_price numeric(7,2));
+discount_price numeric(7,2),alpha int);
 
-CREATE TABLE cars (id serial primary key, price numeric(7,2) default 3.14 );
+CREATE TABLE cars (id serial primary key, price numeric(7,2) default 3.14, alpha int );
 
-INSERT INTO boats (id) values (generate_series(1,3*10e3,1));
-INSERT INTO cars (id) values (generate_series(1,10e4,1));
+INSERT INTO boats (alpha) values (generate_series(1,3*10e3,1));
+INSERT INTO cars (alpha) values (generate_series(1,10e4,1));
